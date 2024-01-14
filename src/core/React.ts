765c535efc30f12d9ce13858e94889ad959ff44e
hildxd <hildxd@outlook.com>
2024-01-14 23:04:48 +0800

    feat: day2

--- src/core/React.ts
+++ src/core/React.ts
@@ -4,6 +4,16 @@ export type ReactElements = {
   props: Props;
 };
 
+type Fiber = {
+  dom: HTMLElement | Text | null;
+  element: ReactElements;
+  parent?: Fiber;
+  sibling?: Fiber;
+  children?: Fiber;
+  type: string;
+  props: Props;
+};
+
 function createTextElement(text: string): ReactElements {
   return {
     type: "TEXT_ELEMENT",
@@ -33,22 +43,70 @@ export function createElement(
   };
 }
 
+let nextFiber: Fiber | undefined = undefined;
 export function render(element: ReactElements, container: HTMLElement | Text) {
-  const dom =
-    element.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type);
+  nextFiber = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    element: element,
+    type: element.type,
+  };
+}
 
-  const isProperty = (key: string) => key !== "children";
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      // @ts-ignore
-      dom[name] = element.props[name];
-    });
+const wookLoop: IdleRequestCallback = (options) => {
+  let deadline = options.timeRemaining();
+  let shouldYield = false;
+  while (!shouldYield && nextFiber) {
+    nextFiber = runUnitOfWork(nextFiber);
+    shouldYield = deadline < 1;
+  }
+  requestIdleCallback(wookLoop);
+};
 
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+requestIdleCallback(wookLoop);
+
+function runUnitOfWork(fiber?: Fiber) {
+  if (!fiber) return;
+  if (!fiber.dom) {
+    const dom = (fiber.dom =
+      fiber.type === "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(fiber.type));
+
+    const isProperty = (key: string) => key !== "children";
+    Object.keys(fiber.props)
+      .filter(isProperty)
+      .forEach((name) => {
+        // @ts-ignore
+        dom[name] = fiber.props[name];
+      });
+    fiber.parent?.dom?.appendChild(dom);
+  }
+
+  let prevChild: Fiber | null = null;
+  fiber.props.children.forEach((child, index) => {
+    const newFiber: Fiber = {
+      dom: null,
+      element: child,
+      type: child.type,
+      props: child.props,
+      children: undefined,
+      sibling: undefined,
+      parent: fiber,
+    };
+    if (index === 0) {
+      fiber.children = newFiber;
+    } else {
+      prevChild!.children = newFiber;
+    }
+    prevChild = newFiber;
+  });
+
+  if (fiber.children) return fiber.children;
+  if (fiber.sibling) return fiber.sibling;
+  return fiber!.parent?.sibling;
 }
 
 export default {
